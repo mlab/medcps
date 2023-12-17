ead0216e2e93fd8e0aad9fdc58de49d78771a16e
J.H <jiyue_he@163.com>
2023-12-17 12:31:01 -0500

    auto image slide show

--- assets/css/image-slider.js
+++ assets/css/image-slider.js
@@ -2,27 +2,38 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    // slideIndex = slideIndex + n;
+    // showSlides(slideIndex);
 }
 
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    // slideIndex = n;
+    // showSlides(slideIndex);
 }
 
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("demo");
+    let demo = document.getElementsByClassName("demo");
     let captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+
+    if (n > slides.length || slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1 || slideIndex < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+        slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < demo.length; i++) {
+        demo[i].className = demo[i].className.replace(" active", "");
     }
+    
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    demo[slideIndex-1].className = demo[slideIndex-1].className + " active";
+    captionText.innerHTML = demo[slideIndex-1].alt;
+
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    slideIndex = slideIndex + 1;
 }
